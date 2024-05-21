7cf1f7d27d76fec74bd89eaf1f70302055edac76
Jordan Plows <jordan@lightapi.com>
2024-05-20 18:47:56 -0700

    dev: janitor commit

--- backend/app/face-detection.py
+++ backend/app/face-detection.py
@@ -2,7 +2,7 @@ import cv2
 from deepface import DeepFace
 
 
-models = ["VGG-Face", "Facenet", "OpenFace", "DeepID", "ArcFace", "Dlib", "SFace"]
+models = ["Facenet"]
 
 DeepFace.stream('database', model_name="models")
 
@@ -12,7 +12,16 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
 
-    gray_frame = cv2.cvtColor(frame)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BAYER_BG2GRAY)
+
+    rgb_frame = cv2.cvtColor(gray_frame, cv2.COLOR_GRAY2BGR)
+
+    faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    for (x, ym, w, h) in faces:
+        face_roi = rgb_frame[y:y + h, x:x + w]
+
+
 
 
 cap.release()
