7da217c07e8704752d4885418f74ccd043654bca
Jordan Plows <jordan@lightapi.com>
2024-05-20 18:44:42 -0700

    dev: janitor commit

--- backend/app/face-detection.py
+++ backend/app/face-detection.py
@@ -1,7 +1,19 @@
-from deepface import DeepFace
 import cv2
+from deepface import DeepFace
 
 
 models = ["VGG-Face", "Facenet", "OpenFace", "DeepID", "ArcFace", "Dlib", "SFace"]
 
 DeepFace.stream('database', model_name="models")
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    gray_frame = cv2.cvtColor(frame)
+
+
+cap.release()
+cv2.destroyAllWindows()
