b126ab0e4f251d8803405c9ebc9e12f7b00e1bd8
Jacob Valdez <jacobfv123@gmail.com>
2024-05-20 19:36:06 -0500

    renamed models to schema

--- backend/app/api/routes/items.py
+++ backend/app/api/routes/items.py
@@ -4,7 +4,14 @@ from fastapi import APIRouter, HTTPException
 from sqlmodel import func, select
 
 from app.api.deps import CurrentUser, SessionDep
-from app.models import Item, ItemCreate, ItemPublic, ItemsPublic, ItemUpdate, Message
+from backend.app.schema import (
+    Item,
+    ItemCreate,
+    ItemPublic,
+    ItemsPublic,
+    ItemUpdate,
+    Message,
+)
 
 router = APIRouter()
 
