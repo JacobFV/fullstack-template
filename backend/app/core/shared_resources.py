3e781728bd67216cfe63940b61fbc9a02a60e2cf
Jordan Plows <jordan@lightapi.com>
2024-05-26 19:39:42 -0700

    dev: janitor commit

--- backend/app/core/shared_resources.py
+++ backend/app/core/shared_resources.py
@@ -3,12 +3,9 @@ import redis
 import redis
 
 def get_redis_connection():
-    """
-    Establishes a connection to a Redis server.
-    
-    Returns:
-        redis.Redis: A Redis client instance connected to the Redis server.
-    """
+   
+   # redis logic here put here later
+
     try:
         # Replace 'localhost' and 6379 with your Redis server's host and port if different
         r = redis.Redis(host='localhost', port=6379, db=0)
@@ -18,7 +15,5 @@ def get_redis_connection():
         raise
 
 settings = {
-    "database_name": "my_database",
-    "api_key": "your_api_key_here",
-    "connection_string": "your_connection_string_here"
+#
 }
