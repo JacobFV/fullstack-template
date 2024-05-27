37da232a00da7350e73f1a4c00c307bff16cb534
Jordan Plows <jordan@lightapi.com>
2024-05-27 09:01:54 -0700

    dev: janitor commit

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -3,14 +3,13 @@ from typing import Any
 from sqlmodel import Session, select
 
 from app.core.security import get_password_hash, verify_password
-from app.schema.proof_of_id_verification import (
+from app.schema.verification.proof_of_id_verification import (
     Item,
     ItemCreate,
     User,
     UserCreate,
     UserUpdate,
 )
-
 
 def create_user(*, session: Session, user_create: UserCreate) -> User:
     db_obj = User.model_validate(
